583ce64b2c04b3d4e00f102e132e668c3914cd6c
niravassar <nirav_p_assar@yahoo.com>
2017-05-03 14:44:39 -0500

    ControllerSpec template with testing-support

--- templates/testing/Controller.groovy
+++ templates/testing/Controller.groovy
@@ -1,12 +1,8 @@
 @artifact.package@
-import grails.test.mixin.TestFor
+import grails.testing.web.controllers.ControllerUnitTest
 import spock.lang.Specification
 
-/**
- * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
- */
-@TestFor(@artifact.name@Controller)
-class @artifact.name@ControllerSpec extends Specification {
+class @artifact.name@ControllerSpec extends Specification implements ControllerUnitTest<@artifact.name@Controller>{
 
     def setup() {
     }
