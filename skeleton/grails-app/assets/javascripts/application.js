16d51b0b4f02e30c291e1f2260fccc78a2e7c0e5
Michael Yan <rain@rainboyan.com>
2022-03-07 17:28:19 +0800

    Rename to jquery.min

--- skeleton/grails-app/assets/javascripts/application.js
+++ skeleton/grails-app/assets/javascripts/application.js
@@ -5,12 +5,12 @@
 // You're free to add application-wide JavaScript to this file, but it's generally better
 // to create separate JavaScript files as needed.
 //
-//= require jquery-3.5.1.min
+//= require jquery.min
 //= require popper.min
 //= require bootstrap
 //= require bs-custom-file-input.min
 //= require_self
 
-$(document).ready(function() {
-    bsCustomFileInput.init();
-});
+// $(document).ready(function() {
+//     bsCustomFileInput.init();
+// });
